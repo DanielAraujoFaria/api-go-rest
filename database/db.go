54f5472ce4e832aa848d3dd740fdd180bce34c33
DanielAraujoFaria <crosscross2.706@gmail.com>
2025-01-30 22:53:48 -0300

    Commit da versão final

---
+++ database/db.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConectaComBancoDeDados() {
+	stringDeConexao := "host=localhost port=5433 user=root dbname=root password=root"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Erro ao conectar com banco de dados")
+	}
+}
